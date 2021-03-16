7cacc9cc000ac893999e120cd59e829f023859ee
Yahir Cardona <yahir.cardona@gmail.com>
2021-03-15 20:26:45 -0600

    console error fixed

--- src/components/PokemonFilter.js
+++ src/components/PokemonFilter.js
@@ -2,12 +2,12 @@ import '../styles/pokemonFilter.css';
 import PropTypes from 'prop-types';
 
 const PokemonFilter = ({ event }) => {
-  const categoryList = ['fire', 'water', 'grass', 'normal', 'electric', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'fairy'];
+  const categoryList = ['All', 'fire', 'water', 'grass', 'normal', 'electric', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'fairy'];
   return (
     <div className="category">
       <span>Select a type of Pokemon</span>
       <select className="categories" name="categories" onChange={event}>
-        <option value="All" selected>All</option>
+        <option value="All">Type</option>
         {categoryList.map(item => <option value={item} key={`${item}_option`}>{item}</option>)}
       </select>
     </div>
